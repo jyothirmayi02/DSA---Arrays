e841923f769e61af2fa5ed60038d0c3fdd3f4f7a
jyothirmayi02 <jyothirmayib2006@gmail.com>
2025-03-02 20:18:58 +0530

    Add files via upload

---
+++ sorted_or_not.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n = nums.size(),count=0;
+        for (int i=0;i<n;i++)
+        {
+            if (nums[i]>nums[(i+1)%n]){
+               count = count+1;
+               if (count>1)
+                 return false;
+            }
+        }
+        return true;
+    }
+};
